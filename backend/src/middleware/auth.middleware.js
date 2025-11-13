6bbafb3a47d4abf01a599cd2d316a18f24c93b4c
sunil <sunilpawar@vebuin.com>
2025-11-13 22:26:29 +0530

    resolving mismatch token name in protect route

--- backend/src/middleware/auth.middleware.js
+++ backend/src/middleware/auth.middleware.js
@@ -5,7 +5,7 @@ import { ENV } from '../../Env.js';
 
 export const protectRoute = async(req, res, next) => {
     try {
-        const token = req.cookies.jwt;
+        const token = req.cookies.token;
         if(!token) return res.status(401).json({ message: "Unauthorized: No token provided" });
             
           const decoded = jwt.verify(token, ENV.JWT_SECRET);
