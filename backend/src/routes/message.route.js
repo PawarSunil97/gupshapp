d1973c5186615ce05463421d911268e5c58a3140
sunil <sunilpawar@vebuin.com>
2025-11-15 23:17:36 +0530

    message send to receiver functionality added

--- backend/src/routes/message.route.js
+++ backend/src/routes/message.route.js
@@ -1,12 +1,12 @@
 import { Router } from "express";
-
+import { getAllContacts, getChatPartners, getMessagesByUserId, sendMessage} from "../controllers/message.controllers.js";
+import { protectRoute } from "../middleware/auth.middleware.js";
+import { arcjetProtection } from "../middleware/arcjet.middleware.js";
 const router = Router();
-
-router.post('/send', (req, res) => {
-    res.send('send message endpoint');
-})
-router.get('/inbox', (req, res) => {
-    res.send('inbox endpoint');
-});
+router.use(arcjetProtection,protectRoute);
+router.get('/contacts',protectRoute, getAllContacts);
+router.get('/chats',protectRoute,getChatPartners);
+router.get('/:id',protectRoute, getMessagesByUserId);
+router.post('/send/:id',protectRoute, sendMessage);
 
 export default router;
